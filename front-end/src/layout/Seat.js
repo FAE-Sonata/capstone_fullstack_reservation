8f24f7d04aa2fc4df535d6fa7e21b033eca25ca0
FAE-Sonata <HeYu1993@gmail.com>
2021-08-15 22:11:08 -0400

    seating submission handler, avoid message for now

--- front-end/src/layout/Seat.js
+++ front-end/src/layout/Seat.js
@@ -3,43 +3,73 @@ import { listTables, readReservation } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
 import { useHistory, useParams } from "react-router";
 
-function Seat(/*reservation_id*/) {
+function Seat() {
   const history = useHistory();
-  const { reservation_id } = useParams();
-  // console.log("SEAT RESERVATION ID: ", reservation_id);
+  const { reservation_id } = useParams(); // reservations/:reservation_id/seat
+  // console.log("SEAT - reservation_id: ", reservation_id);
+
   const [reservation, setReservation] = useState({});
+  const [reservationSize, setReservationSize] = useState(undefined);
   const [tables, setTables] = useState([]);
-  const [reservationError, setReservationError] = useState(null);
-  const [tablesError, setTablesError] = useState(null);
-  useEffect(loadInfo, [reservation_id]);
-  function loadInfo() {
-      const abortController = new AbortController();
-      readReservation(reservation_id, abortController.signal)
-        .then(setReservation)
-        .catch(setReservationError);
-      listTables(abortController.signal)
-        .then(setTables)
-        .catch(setTablesError);
-      return () => abortController.abort();
-  }
-  console.log("FILLED RESERVATION: ", reservation);
-  // console.log("SEAT -- RESERVATION PPL: ", reservation[0]['people']);
+  // const [hasAvailable, setHasAvailable] = useState(true);
+
+  const [errors, setErrors] = useState(null);
+  useEffect(() => {
+    const abortController = new AbortController();
+    // retrieve reservation info for party size
+    async function loadReservation() {
+      try {
+        const reservationResponse = await readReservation(reservation_id,
+          abortController.signal);
+        setReservation(reservationResponse[0]);
+        setReservationSize(reservationResponse[0]['people']);
+        const tablesResponse = await listTables(abortController.signal);
+        setTables(tablesResponse);
+      }
+      catch(error) {
+        setErrors(error);
+      }
+    }
+    loadReservation();
+    return (() => { abortController.abort() });
+  }, [reservation_id]);
+
+  // useEffect(() => {
+  //   let anyOpen = false;
+  //   console.log("USE EFFECT: DETERMINING ANY AVAILABLE TABLES");
+  //   console.log(`${tables.length} TABLES`);
+  //   for(let k = 0; k < tables.length; k++) {
+  //     const thisReservationId = tables[k]['reservation_id'];
+  //     console.log(`TABLE ${k} reservation ID: ${thisReservationId}`);
+  //     console.log(`TABLE ${k} capacity: ${tables[k]['capacity']}`);
+  //     if((!thisReservationId && thisReservationId !== 0) && (
+  //       tables[k]['capacity'] >= reservationSize)) {
+  //         anyOpen = true;
+  //         break;
+  //       }
+  //   }
+  //   if(!anyOpen) setHasAvailable(false);
+  // }, [reservationSize, tables]);
+  // console.log("RESERVATION OBTAINED: ", reservation);
+  // console.log("party size: ", reservationSize);
+  // console.log("TABLES OBTAINED: ", tables);
+
   let tablesOptions = undefined;
-  if(reservation[0] && Object.keys(reservation[0]).length && tables.length) {
-      tables.sort((x,y) => (x['table_name'] > y['table_name']) ? 1 : -1);
-      tablesOptions = tables.map(({table_id, table_name, capacity, reservation_id},
-        index) => (
-          <option key={index} value={table_id} disabled={(
-            reservation_id || reservation_id === 0) || (
-              capacity < reservation[0]['people'])}>
-              {table_name} - {capacity}
-          </option>
-      ));
+  if(reservationSize && tables.length) {
+    tables.sort((x,y) => (x['table_name'] > y['table_name']) ? 1 : -1);
+    tablesOptions = tables.map(({table_id, table_name, capacity, reservation_id},
+      index) => (
+        <option key={index} value={table_id} disabled={(
+          reservation_id || reservation_id === 0) || (
+            capacity < reservationSize)}>
+            {table_name} - {capacity}
+        </option>
+    ));
   }
 
   function getSelected() {
     const selectElem = document.querySelector("select");
-    if(Object.keys(selectElem).length) {
+    if(selectElem && Object.keys(selectElem).length) {
       // console.log("HAS SELECT ELEMENT");
       for(let k = 0; k < selectElem.options.length; k++) {
         const thisOption = selectElem.options[k];
@@ -48,23 +78,54 @@ function Seat(/*reservation_id*/) {
     }
     return undefined;
   }
+
+  const handleSubmit = (event) => {
+    console.log("ENTERING SUBMIT HANDLER");
+    event.preventDefault();
+    const selectedId = getSelected();
+    console.log("SELECTED ID was: ", selectedId);
+    if(selectedId) {
+      const submission = { data: { reservation_id: reservation_id } };
+      const headers = new Headers();
+      headers.append("Content-Type", "application/json");
+
+      const abortController = new AbortController();
+      fetch(`http://localhost:5000/tables/${selectedId}/seat`, {
+        method: 'PUT',
+        headers: headers,
+        body: JSON.stringify(submission),
+        signal: abortController.signal,
+      })
+        .then((res) => res.json())
+        .catch(setErrors);
+      history.push(`../../dashboard`);
+      return;
+    }
+  };
   return (
     <main>
-      <h1>Seating</h1>
-      {/* <div className="d-md-flex mb-3">
-        <p><a href={`?date=${previous(date)}`}>[Previous date]</a></p><br/>
-        <h4 className="mb-0">{`Reservations for the date of ${date}`}</h4><br/>
-        <p><a href={`?date=${next(date)}`}>[Next date]</a></p><br/>
-      </div> */}
-      <ErrorAlert error={reservationError} />
-      <ErrorAlert error={tablesError} />
-      <p>Select a table, format is "(table name) - number of seats":</p>
-      {(tablesOptions) ? (<select name="table_id">
-        {tablesOptions}
-      </select>) : `No open tables matching requested party size of ${(
-        reservation[0]) ? (reservation[0]['people']) : "unknown"}`}
-      <button type="submit" disabled={!getSelected()}>Submit</button>
-      <button onClick={() => history.goBack()}>Cancel</button>
+      <form onSubmit={handleSubmit}>
+        <h1>Seating</h1>
+        {/* <div className="d-md-flex mb-3">
+          <p><a href={`?date=${previous(date)}`}>[Previous date]</a></p><br/>
+          <h4 className="mb-0">{`Reservations for the date of ${date}`}</h4><br/>
+          <p><a href={`?date=${next(date)}`}>[Next date]</a></p><br/>
+        </div> */}
+        {/* <ErrorAlert error={reservationError} />
+        <ErrorAlert error={tablesError} /> */}
+        <ErrorAlert error={errors} />
+        <p>Select a table, format is "(table name) - number of seats":</p>
+        
+          <select name="table_id">
+            {tablesOptions}
+          </select>
+        
+        {/* <button type="submit" disabled={!getSelected()}>Submit</button> */}
+        <button type="submit">Submit</button>
+        <button onClick={() => history.goBack()}>Cancel</button>
+        {/* <p hidden={hasAvailable}>No tables matching party size of {
+          reservationSize} available. Press "Cancel" to go back.</p> */}
+      </form>
     </main>
   );
 }
