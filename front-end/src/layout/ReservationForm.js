4e4a0be50b1d97aef02b1a63cbb90b2751e9761c
FAE-Sonata <HeYu1993@gmail.com>
2021-08-20 13:11:07 -0400

    move time functionality to separate file under front-end/src/utils

--- front-end/src/layout/ReservationForm.js
+++ front-end/src/layout/ReservationForm.js
@@ -1,8 +1,13 @@
 // import moment from "moment";
 import React, { useState } from "react";
 import { useHistory } from "react-router";
-import { today, next } from "../utils/date-time";
-const RANGE_TIMES = ["10:30", "21:30"];
+// import { today, next } from "../utils/date-time";
+import { RANGE_TIMES,
+  earliestHour,
+  latestHour,
+  getOpenOn,
+  getCloseOn,
+  getFirstTopHour } from "../utils/additional-time-functions";
 let serverError = {};
 
 /**
@@ -12,88 +17,6 @@ let serverError = {};
  */
 function padInt(x) {
   return x.toString().padStart(2, "0");
-}
-
-/**
- * 
- * @returns hour (in 24-hour format) of restaurant's opening time
- */
-function earliestHour() {
-  const EARLIEST_SPLIT = RANGE_TIMES[0].split(":").map(x => parseInt(x));
-  return EARLIEST_SPLIT[0];
-}
-
-/**
- * 
- * @returns hour (in 24-hour format) of restaurant's closing time
- */
-function latestHour() {
-  const LATEST_SPLIT = RANGE_TIMES[1].split(":").map(x => parseInt(x));
-  return LATEST_SPLIT[0];
-}
-
-/**
- * 
- * @param {Date} someDate 
- * @returns restaurant's opening time on the specified date
- */
-function getOpenOn(someDate) {
-  const year = someDate.getFullYear();
-  const month = someDate.getMonth();
-  const day = someDate.getDate();
-  const EARLIEST_SPLIT = RANGE_TIMES[0].split(":").map(x => parseInt(x));
-  const EARLIEST_TIME = new Date(year, month, day,
-    EARLIEST_SPLIT[0], EARLIEST_SPLIT[1]);
-  return EARLIEST_TIME;
-}
-
-/**
- * 
- * @param {Date} someDate 
- * @returns restaurant's closing time (earlier than midnight) on the specified date
- */
- function getCloseOn(someDate) {
-  const year = someDate.getFullYear();
-  const month = someDate.getMonth();
-  const day = someDate.getDate();
-  const LATEST_SPLIT = RANGE_TIMES[1].split(":").map(x => parseInt(x));
-  const LATEST_TIME = new Date(year, month, day,
-    LATEST_SPLIT[0], LATEST_SPLIT[1]);
-  return LATEST_TIME;
-}
-
-/**
- * 
- * @returns default time to initially populate the form; either 
- * 1) the opening time of the next day if A) the current date is a Tuesday 
- * or B) the current time is after the closing time
- * 2) the top of the next full hour in the restaurant's operating window
- * 3) the closing time (not a top of the hour, e.g. 23:00) if in the last
- * truncated hour of the restaurant's operating window
- */
-function getFirstTopHour() {
-  const CURRENT_TIME = new Date();
-  if(CURRENT_TIME.getDay() === 2 || CURRENT_TIME > getCloseOn(CURRENT_TIME)) {
-    const tomorrowStr = next(today());
-    const fields = tomorrowStr.split("-");
-    const tomorrowMidnight = new Date(parseInt(fields[0]),
-      parseInt(fields[1])-1, parseInt(fields[2]));
-    return getOpenOn(tomorrowMidnight);
-  }
-  else {
-    const openToday = getOpenOn(CURRENT_TIME);
-    if(CURRENT_TIME < openToday) return openToday;
-    const CURRENT_HOUR = CURRENT_TIME.getHours();
-    if(CURRENT_HOUR < latestHour()) {
-      const topNextHour = new Date(CURRENT_TIME.getFullYear(),
-        CURRENT_TIME.getMonth(),
-        CURRENT_TIME.getDate(),
-        CURRENT_HOUR + 1,
-        0);
-      return topNextHour;
-    }
-    return getCloseOn(CURRENT_TIME);
-  }
 }
 
 function ReservationForm() {
---
+++ front-end/src/utils/additional-time-functions.js
@@ -0,0 +1,83 @@
+import { today, next } from "./date-time";
+export const RANGE_TIMES = ["10:30", "21:30"];
+/**
+ * 
+ * @returns hour (in 24-hour format) of restaurant's opening time
+ */
+export function earliestHour() {
+    const EARLIEST_SPLIT = RANGE_TIMES[0].split(":").map(x => parseInt(x));
+    return EARLIEST_SPLIT[0];
+}
+
+/**
+ * 
+ * @returns hour (in 24-hour format) of restaurant's closing time
+ */
+export function latestHour() {
+    const LATEST_SPLIT = RANGE_TIMES[1].split(":").map(x => parseInt(x));
+    return LATEST_SPLIT[0];
+}
+
+/**
+ * 
+ * @param {Date} someDate 
+ * @returns restaurant's opening time on the specified date
+ */
+export function getOpenOn(someDate) {
+    const year = someDate.getFullYear();
+    const month = someDate.getMonth();
+    const day = someDate.getDate();
+    const EARLIEST_SPLIT = RANGE_TIMES[0].split(":").map(x => parseInt(x));
+    const EARLIEST_TIME = new Date(year, month, day,
+        EARLIEST_SPLIT[0], EARLIEST_SPLIT[1]);
+    return EARLIEST_TIME;
+}
+
+/**
+ * 
+ * @param {Date} someDate 
+ * @returns restaurant's closing time (earlier than midnight) on the specified date
+ */
+export function getCloseOn(someDate) {
+    const year = someDate.getFullYear();
+    const month = someDate.getMonth();
+    const day = someDate.getDate();
+    const LATEST_SPLIT = RANGE_TIMES[1].split(":").map(x => parseInt(x));
+    const LATEST_TIME = new Date(year, month, day,
+        LATEST_SPLIT[0], LATEST_SPLIT[1]);
+    return LATEST_TIME;
+}
+
+/**
+ * 
+ * @returns default time to initially populate the form; either 
+ * 1) the opening time of the next day if A) the current date is a Tuesday 
+ * or B) the current time is after the closing time
+ * 2) the top of the next full hour in the restaurant's operating window
+ * 3) the closing time (not a top of the hour, e.g. 23:00) if in the last
+ * truncated hour of the restaurant's operating window
+ */
+export function getFirstTopHour() {
+    const CURRENT_TIME = new Date();
+    if(CURRENT_TIME.getDay() === 2 || CURRENT_TIME > getCloseOn(CURRENT_TIME)) {
+        const tomorrowStr = next(today());
+        const fields = tomorrowStr.split("-");
+        const tomorrowMidnight = new Date(parseInt(fields[0]),
+        parseInt(fields[1])-1, parseInt(fields[2]));
+        return getOpenOn(tomorrowMidnight);
+    }
+    else {
+        const openToday = getOpenOn(CURRENT_TIME);
+        if(CURRENT_TIME < openToday) return openToday;
+        const CURRENT_HOUR = CURRENT_TIME.getHours();
+        if(CURRENT_HOUR < latestHour()) {
+        const topNextHour = new Date(CURRENT_TIME.getFullYear(),
+            CURRENT_TIME.getMonth(),
+            CURRENT_TIME.getDate(),
+            CURRENT_HOUR + 1,
+            0);
+        return topNextHour;
+        }
+        return getCloseOn(CURRENT_TIME);
+    }
+}
